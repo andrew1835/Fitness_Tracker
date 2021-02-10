c2e04151d0e099b1029eef2742d7992d835645b1
Andrew Anthony <a.anthony1835@gmail.com>
2021-02-10 01:24:43 -0800

    Finished server.js file. Only have to finish script.js and it should be fully functional

--- server.js
+++ server.js
@@ -26,7 +26,58 @@ app.get('/', (req, res) => {
     res.sendFile('./index.html')
 })
 
+app.get('/api/exercises', (req, res) => {
+    db.Exercise.find({})
+        .then(dbExercises => {
+            res.json(dbExercises);
+        })
+})
 
+app.get('/api/workouts', (req, res) => {
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            console.log(err)
+            res.send(err);
+        })
+})
+
+app.get('/populatedexercises', (req, res) => {
+    db.Workout.find({})
+        .populate('exercises')
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            console.log(err)
+            res.send(err);
+        })
+})
+
+app.post('/api/workouts', ({ body }, res) => {
+    db.Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            console.log(err)
+            res.send(err);
+        })
+})
+
+app.post('/api/exercises', (req, res) => {
+    console.log(req.body);
+
+    db.Exercise.create(req.body)
+        .then(dbExercise => {
+            db.Workout.findOneAndUpdate({ _id: req.body.workoutId }, { $push: { exercises: dbExercise._id } })
+                .then(dbWorkout => res.send(dbWorkout))
+        })
+        .catch(err => res.json(err))
+
+})
 
 
 
